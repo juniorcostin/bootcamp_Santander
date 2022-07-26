03c5c5aa6d5bdcd114ea0a7ab55277f6ea8e3a38
João Carlos Costin Junior <junior19costin@gmail.com>
2022-07-26 20:04:29 -0300

    Aula de módulos Javascript

---
+++ JavaScript/Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("João", 22))
+console.log(mostraCidade("João", "Curitiba"))
+console.log(mostraHobby("João", "Jogar"))
